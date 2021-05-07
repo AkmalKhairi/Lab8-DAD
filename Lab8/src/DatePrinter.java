0a39bf736faf7308dd68608fd5c2f5b6331f4126
AkmalKhairi <73923156+AkmalKhairi@users.noreply.github.com>
2021-05-07 17:44:02 +0800

    Update DatePrinter.java

--- Lab8/src/DatePrinter.java
+++ Lab8/src/DatePrinter.java
@@ -2,19 +2,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatePrinter extends Thread {
-	public void printDate () {
+	public void printDate (String Threads) {
 
-		for (int counter = 0; counter < 5; counter++) {
+		for (int counter = 0; counter < 10; counter++) {
 
 			SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");  
 		    Date date = new Date();  
-		    System.out.println("Date Printer " + formatter.format(date));  
-		}
+		    System.out.println(Threads + "= Date Printer " + formatter.format(date));		}
 		
 	}
 	
 	public void run() {
-		printDate();
+		Thread Threads = Thread.currentThread();
+		printDate(Threads.getName());
 		
 		}
 
